58ed91cbe9be2c650752e35bc47aa82b96f11f20
Pete Favelle <ahnlak@gmail.com>
2014-05-12 10:05:23 +0100

    Replaced the 'category' selection dropdown for a more generic one, allowing users to select category, tag, or any othe custom taxonomy appropriate for the selected post type.

---
+++ includes/ajax_handler.js
@@ -0,0 +1,26 @@
+function tax_term_postback( select_id, post_type ) {
+  var data = {
+    action: 'tax_term_action',
+	post_type: post_type
+  };
+  jQuery.post(ajax_object.ajax_url, data, function(response) {
+	// Decode the data received.
+	var terms = jQuery.parseJSON(response);
+
+	// Keep track of what was previously selected
+	var select_ctrl = jQuery('#' + select_id);
+	var old_term = select_ctrl.val();
+
+	// Clear out the old options, build up the new
+	select_ctrl.empty();
+	jQuery.each(terms, function(key, value) {
+		var new_option = jQuery('<option></option>')
+					    .attr('style', 'padding-right:10px;')
+					    .attr('value', value).text(value);
+		if (value == old_term) {
+			new_option.attr('selected', true);
+		}
+		select_ctrl.append(new_option);
+	});
+  });
+}
